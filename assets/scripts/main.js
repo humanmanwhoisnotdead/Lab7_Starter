a449d8b4f267f1aa4d8aea5e7239e330512f388d
Christopher Kroon <ckroon@ucsd.edu>
2021-11-04 21:18:03 -0700

    C2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,6 @@
 // main.js
 
-import { Router } from './router.js';
+import { Router } from 'https://raw.githubusercontent.com/humanmanwhoisnotdead/Lab6_Starter/main/assets/scripts/router.js';
 
 const recipes = [
   'https://introweb.tech/assets/json/ghostCookies.json',
